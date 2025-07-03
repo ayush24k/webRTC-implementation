60dcad9e130ee54811c4b28eef4dbe157fe8ccd7
ayush24k <ayushanshu69@gmail.com>
2025-07-03 19:44:51 +0530

    minor fronetnd changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,10 +4,23 @@ import Receiver from "./pages/receiverPage"
 
 function App() {
 
+  function Home() {
+    return (
+      <div>
+        <h1>Home Page</h1>
+        <nav>
+          <Link to={'/sender'} target="_blank">sender page</Link>
+          <br></br>
+          <Link to={'/receiver'} target="_blank">receiver page</Link>
+        </nav>
+      </div>
+    )
+  }
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Link to={'/sender'}>home page</Link>}></Route>
+        <Route path="/" element={<Home />}></Route>
         <Route path="/sender" element={<Sender />}></Route>
         <Route path="/receiver" element={<Receiver />}></Route>
       </Routes>
